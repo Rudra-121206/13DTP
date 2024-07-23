4019731917ded8f51a38e6a2d79d8339748019f9
none <patha@Rudraslaptop.com>
2024-07-24 11:33:57 +1200

    Setting up project

--- CHATBOX PROJECT/app/models.py
+++ CHATBOX PROJECT/app/models.py
@@ -1,37 +1,45 @@
 from app.routes import db
 
-Classes_Person=db.Table('Classes_Person', 
-    db.Column('', db.Integer, db.ForeignKey('')),
-    db.Column('', db.Integer, db.ForeignKey(''))
+Classes_Person = db.Table('Classes_Person', 
+    db.Column('person_id', db.Integer, db.ForeignKey('Person.person_id')),
+    db.Column('classes_id', db.Integer, db.ForeignKey('Classes.class_id'))
 ) 
 
-class Person(db.Model):
-    __tablename__="Person"
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.Text())
-    email=db.Column(db.text())
-    password=db.Column(db.text())
-    subject=db.Column(db.text())
-    role=db.Column(db.text())
-    year_level=db.Column(db.text())
 
+class Person(db.Model):
+    __tablename__ = "Person"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text())
+    email = db.Column(db.text())
+    password = db.Column(db.text())
+    subject = db.Column(db.text())
+    role = db.Column(db.text())
+    year_level = db.Column(db.text())
+    classes = db.relationship("Chats", back_populates="Class", cascade="all, delete-orphan")
+    Classes = db.relationship("Classes",
+        secondary="Classes_Person",
+        back_populates='person')
 
 
 class Classes(db.Model):
-    __tablename__="Classes"
-    class_id=db.Column(db.integer, primary_key=True)
-    joining_code=db.column(db.integer())
-    subject=db.column(db.Text())
-    year_level=db.Column(db.text())
+    __tablename__ = "Classes"
+    class_id = db.Column(db.integer, primary_key=True)
+    joining_code = db.column(db.integer())
+    subject = db.column(db.Text())
+    year_level = db.Column(db.text())
+    persons = db.relationship("Chats", back_populates="person", cascade="all, delete-orphan")
+    person = db.relationship("Person",
+        secondary="Classes_person",
+        back_populates="Classes")
 
 
 class Chats(db.Model):
-    __tablename__="Chats"
-    chats_id=db.Column(db.integer, primary_key=True)
-    person_id=db.Column(db.integer, db.ForeignKey("Person.id"))
-    classes_id=db.Column(db.integer, db.ForeignKey("Classes.class_id"))
-    = db.relationship('Actor', back_populates='movies')
-    movie = db.relationship('Movie', back_populates='actors')
+    __tablename__ = "Chats"
+    chats_id = db.Column(db.integer, primary_key=True)
+    person_id = db.Column(db.integer, db.ForeignKey("Person.person_id"))
+    classes_id = db.Column(db.integer, db.ForeignKey("Classes.class_id"))
+    person = db.relationship('Person', back_populates='classes')
+    Class = db.relationship('Classes', back_populates='persons')
 
 
 
--- CHATBOX PROJECT/app/routes.py
+++ CHATBOX PROJECT/app/routes.py
@@ -2,9 +2,17 @@ from app import app
 from flask import render_template, abort
 from flask_sqlalchemy import SQLAlchemy # no more boring old SQL for us!
 import os
+import app.models as models
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "chatbox.db")
 db.init_app(app)
 
+
+@app.route('/all_classes')
+def all_classes():
+    all_classes = models.Classes.query.all()
+    return render_template('all_classes.html', all_classes=all_classes)
+
+ 
